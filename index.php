58a2f9b35190a17e501b6c495491013d1a7668d5
coder-ms <marco.sbrascia@gmail.com>
2022-12-12 14:42:29 +0200

    aggiunta public function

--- index.php
+++ index.php
@@ -6,14 +6,46 @@ class Movie{
     // DATI PRINCIPALI
     public $name;
     public $year;
+    public $genre;
     public $runningTime;
     private $actors;
-    public static $startingPrice;
+    private $age;
+
+    // variabili calcolate
+    public $discountTicket;
+    public static $startingPrice = 5;
 
 
-    function __construct($_name, $_year, $_actors = []){
-        
+    function __construct($_name, $_year, $_genre, $_actors = []){
+        $this->name = $_name;
+        $this->year = $_year;
+        $this->genre = $_genre;
+        $this->actors = $_actors;
+
+        // operazioni necessarie durante l'instanziamento
+        $this->setDiscount();
+  }
+
+
+
+  // in base all'età, si generi uno sconto
+  // se età > 65, lo sconto è del 40%
+  // se età < 18, lo sconto è del 20%
+  public function setDiscount() {
+    if($this->age > 65) {
+
+      $this->discountTicket = 40;
+
+    } 
+    else if($this->age < 65){
+        $this->discountTicket = 20;
     }
+    else {
+
+      $this->discountTicket = 0;
+
+    }
+  }
 
 }
 
