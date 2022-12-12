d5cc387d292a914defdabf68b86a1f49befee8b3
coder-ms <marco.sbrascia@gmail.com>
2022-12-12 14:04:57 +0200

    dati principali

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// echo 'Hello world';
+
+/* CREAZIONE CLASSE */
+class Movie{
+    // DATI PRINCIPALI
+    public $name;
+    public $year;
+    public $runningTime;
+    private $actors;
+    public static $startingPrice;
+
+
+
+
+}
+
+echo '($name, $year, $runningTime)';
+
+?>
